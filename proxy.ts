df91af2ec91db35c3c20160633e2ab2ade93ee71
Ashwin Preyan M <ashwinpreyan@gmail.com>
2026-02-02 17:00:07 +0530

    some changes

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   if (pathname.startsWith("/franchisee")) {
