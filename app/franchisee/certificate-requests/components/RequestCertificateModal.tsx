6dba3dc8c9d8e45fc7daa8e0cb0d6c2545e56347
Ashwin Preyan M <ashwinpreyan@gmail.com>
2026-02-09 18:14:32 +0530

    lint fix

--- app/franchisee/certificate-requests/components/RequestCertificateModal.tsx
+++ app/franchisee/certificate-requests/components/RequestCertificateModal.tsx
@@ -185,7 +185,7 @@ export default function RequestCertificateModal({
               id="marksObtained"
               type="number"
               min="0"
-              value={formData.marksObtained === "0" || formData.marksObtained === 0 ? "" : formData.marksObtained}
+              value={formData.marksObtained === "0" ? "" : formData.marksObtained}
               onChange={(e) =>
                 handleInputChange("marksObtained", e.target.value)
               }
